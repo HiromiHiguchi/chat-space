38fb1476e372e78d70e7989135a2aeb92c0b7a8c
樋口宏美 <kinetan.722@gmail.com>
2019-08-14 15:05:36 +0900

    add_ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,45 @@
+$(function(){
+  function buildHTML(message){
+  var html = `<div class="contents__chat-right-side__messages__message">
+                <div class="contents__chat-right-side__messages__message__upper-info">
+                    <div class="contents__chat-right-side__messages__message__upper-info__talker">
+                        ${ message.user_name }
+                    </div>
+                    <div class="contents__chat-right-side__messages__message__upper-info__date">
+                        ${ message.time }
+                    </div>
+                  </div>
+                </div>
+                <div class="contents__chat-right-side__messages__message__text">
+                    <p class="contents__chat-right-side__messages__message__text">
+                        ${ message.content }
+                    </p>
+                </div>`
+        return html; 
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+      })
+
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.contents__chat-right-side__messages').append(html);
+        $('.form__message').val('');
+        $('.form__submit').prop('disabled', false);
+        $('.contents__chat-right-side__messages').animate({ scrollTop: $('.contents__chat-right-side__messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function(){
+        alert('error')
+        $('.form__submit').prop('disabled', false);
+    })
+  })
+})
