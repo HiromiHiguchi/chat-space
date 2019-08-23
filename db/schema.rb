50980945181f648d28bbd7eeab550c0ea598172c
樋口宏美 <kinetan.722@gmail.com>
2019-08-23 12:12:14 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,9 @@ ActiveRecord::Schema.define(version: 20190813033140) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["name"], name: "index_users_on_name", using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "group_users", "groups"
